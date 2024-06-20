41f330f639f9dbacde8d01766f1e4eb9217c782b
Fabio <fabio@f2sc.com>
2024-06-20 11:48:03 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Por favor ingrese un nombre");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su cargo");
+string cargo = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su edad");
+string edad = Console.ReadLine();
+
+Console.WriteLine($"Hola buenas tardes! Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad} años.");
