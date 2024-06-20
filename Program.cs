cc0ce5910cc935199a23f55a5aac61c7ac5b3de8
Fabio <fabio@f2sc.com>
2024-06-20 12:57:39 +0200

    Feat: Añadida librería Humanizer al proyecto desde Nuget

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿using Humanizer;
+using System.Globalization;
+
+Console.WriteLine($"Bienvenido al año  {DateTime.Now.Year.ToWords(new CultureInfo("es"))} o como se diría en Portugués: {DateTime.Now.Year.ToWords(new CultureInfo("pt"))}");
+
 Console.WriteLine("Por favor ingrese un nombre");
 string nombre = Console.ReadLine();
 
@@ -6,6 +10,7 @@ Console.WriteLine("Por favor ingrese su cargo");
 string cargo = Console.ReadLine();
 
 Console.WriteLine("Por favor ingrese su edad");
-string edad = Console.ReadLine();
+int edad = int.Parse( Console.ReadLine());
 
-Console.WriteLine($"Hola buenas tardes! Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad} años.");
+Console.WriteLine($"Hola buenas tardes! Mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad.ToWords(new CultureInfo("cn"))} años.");
+
